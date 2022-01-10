24d1ef84d4cc68ac4e6edbe99877cd3627dcb94a
Yoonji Shin <gbionoak01@gmail.com>
2022-01-11 00:04:03 +0900

    문자 찾기

---
+++ src/String1.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+
+/**
+ * @author YJ
+ *
+ * 1. 입력받은 문자열과 문자를 대문자 혹은 소문자로 변환
+ * 2. for문을 통해 문자열과 문자를 비교하여 개수 세기
+ *
+ */
+
+class Main{
+    public int solution(String str, char t){
+        int answer=0;
+
+        // 1.
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+
+        // 2.
+        for (int i=0; i<str.length(); i++) {
+            if(str.charAt(i) == t)	answer++;
+        }
+
+        System.out.println(answer);
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner scan = new Scanner(System.in);
+        String str=scan.next();
+        char ch = scan.next().charAt(0);
+
+        System.out.print(T.solution(str, ch));
+    }
+}
