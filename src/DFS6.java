699549d4869f2237b9a1ab00949b756e7cb28f9f
Yoonji Shin <gbionoak01@gmail.com>
2022-01-25 23:16:21 +0900

    부분집합 구하기

--- src/DFS6.java
+++ src/DFS6.java
@@ -1,4 +1,29 @@
 
 public class DFS6 {
+	static int n;
+	static int[] ch;
+	public void DFS(int L) {
+		if(L==n+1) {	// 말단 노드에 도착
+			String tmp = "";
+			for(int i=1; i<=n; i++) {
+				if(ch[i]==1) 	
+					tmp += (i+" ");
+			}
+			if(tmp.length()>0)	
+				System.out.println(tmp);
+		}else {
+			ch[L]=1;
+			DFS(L+1);	// 왼쪽으로 뻗음
+			ch[L]=0;
+			DFS(L+1);	// 오른쪽으로 뻗음
+		}
+	}
+	
+	public static void main(String args[]) {
+		DFS6 T = new DFS6();
+		n=3;
+		ch=new int[n+1];
+		T.DFS(1); 
+	}
 
 }
