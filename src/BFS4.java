738f0e8d092e471a4848e8040f3877647b9e8356
Yoonji Shin <gbionoak01@gmail.com>
2022-02-17 23:48:29 +0900

    그래프 최단거리(BFS)

--- src/BFS4.java
+++ src/BFS4.java
@@ -1,4 +1,47 @@
+import java.util.*; 
 
 public class BFS4 {
- 
+	static int n, m = 0;
+	static ArrayList<ArrayList<Integer>> graph;
+	static int[] ch,dis;
+	
+	public void BFS(int v) {
+		Queue<Integer> q = new LinkedList<>();
+		ch[v] = 1;
+		dis[v] = 0;
+		q.offer(v);
+		
+		while(!q.isEmpty()) {
+			int cv = q.poll();
+			for(int nv:graph.get(cv)) {
+				if(ch[nv] == 0) {
+					ch[nv] = 1;
+					q.offer(nv);
+					dis[nv] = dis[cv]+1;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		BFS4 T = new BFS4();
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextInt();
+		m = scan.nextInt();
+		graph = new ArrayList<ArrayList<Integer>>();
+		for(int i=0; i<=n; i++) {
+			graph.add(new ArrayList<Integer>());
+		}
+		ch = new int[n+1];
+		dis = new int[n+1];
+		for(int i=0; i<m; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			graph.get(a).add(b);
+		}
+		T.BFS(1);
+		for(int i=2; i<=n; i++) {
+			System.out.println(i + " : " + dis[i]);
+		}
+	}
 }
