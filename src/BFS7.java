3a028ae576f096ede7c9905229b7ffc4d8cb0cce
Yoonji Shin <gbionoak01@gmail.com>
2022-02-26 04:46:43 +0900

    토마토

---
+++ src/BFS7.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+class Point2{
+	public int x,y;
+	
+	Point2(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+}
+
+public class BFS7 {
+	static int m,n;
+	static int[][] board, date;
+	static int[] dx = {-1,0,1,0};
+	static int[] dy = {0,-1,0,1};
+	static Queue<Point> q = new LinkedList<>();
+	
+	static void BFS() {
+		while(!q.isEmpty()) {
+			Point tmp = q.poll();
+			
+			for(int i=0;i<4;i++) {
+				int nx = tmp.x + dx[i];
+				int ny = tmp.y + dy[i];
+				
+				if(nx>=0 && nx<n && ny>=0 && ny<m && board[nx][ny]==0) {	//이해안감
+					board[nx][ny] = 1;
+					q.offer(new Point(nx,ny));
+					date[nx][ny] = date[tmp.x][tmp.y]+1;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		BFS7 T = new BFS7();
+		Scanner scan = new Scanner (System.in);
+		m = scan.nextInt();
+		n = scan.nextInt();
+		board = new int[n][m];
+		date = new int[n][m];
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				board[i][j] = scan.nextInt(); 
+				if(board[i][j]==1)
+					q.offer(new Point(i,j));	//익은 토마토일 경우(시작점)
+			}
+		}
+		T.BFS();
+		
+		boolean flag = true;
+		int answer = Integer.MIN_VALUE;
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {	// 익지 않은 토마토 있음
+				if(board[i][j]==0)
+					flag = false;
+			}
+		}
+		
+		if(flag) {	// 토마토가 다 익은 경우
+			for(int i=0; i<n; i++) {
+				for(int j=0; j<m; j++) {	
+					answer = Math.max(answer, date[i][j]);
+				}
+			}
+			System.out.println(answer);
+		}else 
+			System.out.println(-1);
+	}
+
+}
