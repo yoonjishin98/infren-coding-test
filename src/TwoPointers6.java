9f1dbfecd75390d70ba519b9aabb9b8e7f1b82c1
Yoonji Shin <gbionoak01@gmail.com>
2022-03-18 19:56:10 +0900

    연속된 자연수의 합(수학)

--- src/TwoPointers6.java
+++ src/TwoPointers6.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
 
 public class TwoPointers6 {
-
+	public int solution(int n) {
+		int answer=0, cnt=1;
+		n--;
+		while(n>0) {
+			cnt++;
+			n=n-cnt;
+			if(n%cnt==0) 
+				answer++;
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		TwoPointers6 T = new TwoPointers6();
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		System.out.print(T.solution(n));
+	}
 }
